496e56f8d50dba4cc3717bf5106b1bd6049e82df
devjanaprime <dev@primeacademy.io>
2017-06-19 13:56:29 -0500

    part2

--- modules/routes/listings.js
+++ modules/routes/listings.js
@@ -1,9 +1,27 @@
 var express = require( 'express' );
 var router = express.Router();
+var bodyParser = require( 'body-parser' );
+var mongoose = require( 'mongoose' );
+
+mongoose.connect( 'localhost:27017/realestate' );
+
+var listingsSchema = new mongoose.Schema({
+  city : String,
+  cost : Number,
+	rent : Number,
+  sqft : Number
+}); // end schema
+
+var listings = mongoose.model( 'listings', listingsSchema );
+
+router.use( bodyParser.urlencoded( { extended: true } ) );
+router.use( bodyParser.json() );
 
 router.get( '/', function( req, res ){
   console.log( '/listings get call' );
-  res.send( 'honk' );
+  listings.find().then( function( results ){
+    res.send( results );
+  }); // end find
 }); // end /listings get
 
 module.exports = router;
--- public/scripts/listingsController.js
+++ public/scripts/listingsController.js
@@ -1,14 +1,47 @@
 var myApp = angular.module( 'myApp', [] );
 
+var ALL = 0;
+var RENT = 1;
+var SALE = 2;
+
+
 myApp.controller( 'ListingsController', function( ListingsService ){
   var vm = this;
+  vm.all =[];
+  vm.rent = [];
+  vm.sale = [];
 
   vm.getListings = function(){
     console.log( 'in controller getListings' );
     ListingsService.retrieveListings().then( function(){
       vm.data = ListingsService.data;
+      vm.all = vm.data;
+      for (var i = 0; i < vm.all.length; i++) {
+        if( vm.all[i].cost ){
+          vm.sale.push( vm.all[i] );
+        }
+        else{
+          vm.rent.push( vm.all[i] );
+        }
+      }; // end for
       console.log( 'in controller:', vm.data );
     }); // end service function call
   };//end getListings
+
+  vm.setShow = function( showType ){
+    console.log( 'in setShow:', showType );
+    if( showType == ALL ){
+      vm.data = vm.all;
+    }
+    else if( showType == RENT ){
+      vm.data = vm.rent;
+    }
+    else if( showType == SALE ){
+      vm.data = vm.sale;
+    }
+    else{
+      showType = 0;
+    }
+  }; // end setShow
 
 }); // end controller
